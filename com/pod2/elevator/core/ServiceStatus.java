f91f9c7e968ed75829b2da198fe130950750b8c4
eece419.pod2@gmail.com <eece419.pod2@gmail.com@7da63e08-be2e-2b6b-deee-ad8a1a9b923f>
2010-10-23 05:06:04 +0000

    Hm...another stupid upload

--- com/pod2/elevator/core/ServiceStatus.java
+++
@@ -1,7 +0,0 @@
-package com.pod2.elevator.core;
-
-public enum ServiceStatus {
-	OutOfService,
-	InService,
-	Failed
-}
--- com/pod2/elevator/view/SimulatorView.java
+++
@@ -1,5 +0,0 @@
-package com.pod2.elevator.view;
-
-public class SimulatorView {
-
-}
