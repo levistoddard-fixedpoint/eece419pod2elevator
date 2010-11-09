b13553e30295ed3b5a3f863921b7cd4fbad18d07
levi.stoddard <levi.stoddard@7da63e08-be2e-2b6b-deee-ad8a1a9b923f>
2010-11-09 17:48:05 +0000

    Commenting out some code, and adding google collections library so that code can compile.

--- src/com/pod2/elevator/scheduling/FirstComeFirstServe.java
+++ src/com/pod2/elevator/scheduling/FirstComeFirstServe.java
@@ -1,27 +1,35 @@
 package com.pod2.elevator.scheduling;
 
-public class FirstComeFirstServe extends ElevatorScheduler{
+import com.pod2.elevator.core.ActiveSimulation;
+
+public class FirstComeFirstServe implements ElevatorScheduler{
 	
 	private static long requestNum = 0;
 	private int nextElevator;
 	
 	public void schedule(ActiveSimulation activeSimulation) {
-		//Search all floors from bottom to top for requests
-		for (int i = 0; i < activeSimulation.numberFloors; i++) {
-			// check if button is pressed
-			if (activeSimulation.floorRequestButtons[i].upSelectedQuantum == activeSimulation.currentQuantum || activeSimulation.floorRequestButtons[i].downSelectedQuantum == activeSimulation.currentQuantum) {
-				// assign next elevator to passenger
-				requestNum++;
-				// check to see available elevators
-				do {
-					nextElevator = requestNum % activeSimulation.numberElevators;
-					// if elevator is not in service, increment counter and use next available elevator
-					if (activeSimulation.elevators[nextElevator].serviceStatus == ServiceStatus.InService);
-						break;
-					requestNum++;
-				} while (true);
-				activeSimulation.elevators[nextElevator].moveToFloor(i);
-			}
-		}
+//		//Search all floors from bottom to top for requests
+//		for (int i = 0; i < activeSimulation.numberFloors; i++) {
+//			// check if button is pressed
+//			if (activeSimulation.floorRequestButtons[i].upSelectedQuantum == activeSimulation.currentQuantum || activeSimulation.floorRequestButtons[i].downSelectedQuantum == activeSimulation.currentQuantum) {
+//				// assign next elevator to passenger
+//				requestNum++;
+//				// check to see available elevators
+//				do {
+//					nextElevator = requestNum % activeSimulation.numberElevators;
+//					// if elevator is not in service, increment counter and use next available elevator
+//					if (activeSimulation.elevators[nextElevator].serviceStatus == ServiceStatus.InService);
+//						break;
+//					requestNum++;
+//				} while (true);
+//				activeSimulation.elevators[nextElevator].moveToFloor(i);
+//			}
+//		}
+	}
+
+	@Override
+	public String getKey() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
