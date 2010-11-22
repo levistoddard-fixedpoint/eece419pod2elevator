e01b6329914f5127b6ea50e43301a46282aee3d6
williamLHchang@gmail.com <williamLHchang@gmail.com@7da63e08-be2e-2b6b-deee-ad8a1a9b923f>
2010-11-22 00:58:26 +0000

    Uploaded Missing Files - LogMessage - Menu - Toolbar

---
+++ src/com/pod2/elevator/view/model/Menu.java
@@ -0,0 +1,42 @@
+package com.pod2.elevator.view.model;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class Menu extends JMenuBar {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4942799975819603731L;
+
+	public Menu() {
+		setBorder(BorderFactory.createEtchedBorder());
+
+		ImageIcon iconExit = new ImageIcon("icons/exit.png");
+
+		JMenu file = new JMenu("File");
+		file.setMnemonic(KeyEvent.VK_F);
+
+		JMenuItem fileClose = new JMenuItem("Close", iconExit);
+		fileClose.setMnemonic(KeyEvent.VK_C);
+		fileClose.setToolTipText("Exit application");
+		fileClose.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				System.exit(0);
+			}
+		});
+
+		file.add(fileClose);
+
+		add(file);
+
+	}
+}
