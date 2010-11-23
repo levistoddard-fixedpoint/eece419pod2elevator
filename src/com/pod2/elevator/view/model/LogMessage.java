35d5a05431acb37d8f051d9ab109634a28d6a8f4
williamLHchang@gmail.com <williamLHchang@gmail.com@7da63e08-be2e-2b6b-deee-ad8a1a9b923f>
2010-11-23 04:38:10 +0000

    Adding LogMessage again...not sure what happened ConfigurationView - Changed newPort to JFormattedTextField so it only takes number inputs

---
+++ src/com/pod2/elevator/view/model/LogMessage.java
@@ -0,0 +1,15 @@
+package com.pod2.elevator.view;
+
+public class LogMessage {
+
+	private String message;
+
+	public LogMessage(String message) {
+		this.message = message;
+	}
+
+	public String toString() {
+		return message;
+	}
+	
+}
