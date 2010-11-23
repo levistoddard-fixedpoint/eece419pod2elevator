370bdaa299bf645302d324a9c7754c57353fc86e
williamLHchang@gmail.com <williamLHchang@gmail.com@7da63e08-be2e-2b6b-deee-ad8a1a9b923f>
2010-11-23 04:44:20 +0000

    fixed some package errors

--- src/com/pod2/elevator/core/ResultsBuilder.java
+++ src/com/pod2/elevator/core/ResultsBuilder.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import java.util.Collection;
 
 import com.pod2.elevator.core.events.Event;
-import com.pod2.elevator.view.LogMessage;
+import com.pod2.elevator.view.data.LogMessage;
 
 /**
  * Aggregates all data resulting from an ActiveSimulation.
@@ -20,7 +20,7 @@ public interface ResultsBuilder {
 
 	public void logRequestStateChange(long quantum, RequestInTransit request);
 
-	public Collection<LogMessage> getLogEntries(long quantum);
+	public Collection<com.pod2.elevator.view.data.LogMessage> getLogEntries(long quantum);
 
 	public void onEnd(long quantum);
 
