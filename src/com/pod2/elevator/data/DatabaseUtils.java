9336cd27552422c381da41a56c8e15ab1ac3eeeb
levi.stoddard <levi.stoddard@7da63e08-be2e-2b6b-deee-ad8a1a9b923f>
2010-11-27 03:05:14 +0000

    Adding DatabaseUtils and parsing of database parameters.

--- src/com/pod2/elevator/data/DatabaseUtils.java
+++ src/com/pod2/elevator/data/DatabaseUtils.java
@@ -1,7 +1,41 @@
 package com.pod2.elevator.data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DatabaseUtils {
 
-	
-	
+	private static String user;
+	private static String password;
+	private static String url;
+
+	public static void initialize(Properties properties)
+			throws InvalidDatabaseConfigurationException {
+		final String USER_KEY = "user";
+		final String PASSWORD_KEY = "password";
+		final String URL_KEY = "url";
+
+		if (properties.getProperty(USER_KEY) == null) {
+			throw new InvalidDatabaseConfigurationException(
+					"User name must be specified in database configuration.");
+		}
+		user = properties.getProperty(USER_KEY);
+		if (properties.getProperty(PASSWORD_KEY) == null) {
+			throw new InvalidDatabaseConfigurationException(
+					"Password must be specified in database configuration.");
+		}
+		password = properties.getProperty(PASSWORD_KEY);
+		if (properties.getProperty(URL_KEY) == null) {
+			throw new InvalidDatabaseConfigurationException(
+					"Connection must be specified in database configuration.");
+		}
+		url = properties.getProperty(URL_KEY);
+	}
+
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(url, user, password);
+	}
+
 }
