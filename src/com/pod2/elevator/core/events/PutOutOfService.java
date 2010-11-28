f71c6b42eb9e9bb883a56ab3e22f9caefb453b97
levi.stoddard <levi.stoddard@7da63e08-be2e-2b6b-deee-ad8a1a9b923f>
2010-11-28 17:54:41 +0000

    Resolving issue 13. A PutOutOfService event can now  be applied whenever an elevator is empty, and it will immediately go out of service.

--- src/com/pod2/elevator/core/events/PutOutOfService.java
+++ src/com/pod2/elevator/core/events/PutOutOfService.java
@@ -2,23 +2,23 @@ package com.pod2.elevator.core.events;
 
 import com.pod2.elevator.core.ActiveSimulation;
 import com.pod2.elevator.core.Elevator;
-import com.pod2.elevator.core.MotionStatus;
 
 public class PutOutOfService extends ElevatorEvent {
 	/**
 	 * OVERVIEW: An ElevatorEvent which takes an Elevator out of service.
 	 */
-	
+
 	private final String reason;
 
-	PutOutOfService(EventSource source, long quantum, int elevatorNumber,
-			String reason) {
+	PutOutOfService(EventSource source, long quantum, int elevatorNumber, String reason) {
 		/**
-		 * 	REQUIRES: source != null && quantum >= 0 && elevatorNumber >= 0 &&
-		 * 		reason != null
-		 * 	MODIFIES: this
-		 * 	EFFECTS: Constructor for the class.  Initialize variables for use in
-		 * 		this class.
+		 * REQUIRES: source != null && quantum >= 0 && elevatorNumber >= 0 &&
+		 * reason != null
+		 * 
+		 * MODIFIES: this
+		 * 
+		 * EFFECTS: Constructor for the class.
+		 * Initialize variables for use in this class.
 		 */
 		super(source, quantum, elevatorNumber);
 		assert (reason != null);
@@ -28,9 +28,12 @@ public class PutOutOfService extends ElevatorEvent {
 	@Override
 	public void apply(ActiveSimulation simulation) {
 		/**
-		 * 	REQUIRES: simulation != null
-		 * 	MODIFIES: simulation
-		 * 	EFFECTS: Apply service event to elevator out of service.
+		 * REQUIRES: simulation != null
+		 * 
+		 * MODIFIES: simulation
+		 * 
+		 * EFFECTS: Apply
+		 * service event to elevator out of service.
 		 */
 		Elevator elevator = simulation.getElevators()[getElevatorNumber()];
 		elevator.putOutOfService();
@@ -39,18 +42,15 @@ public class PutOutOfService extends ElevatorEvent {
 	@Override
 	public boolean canApplyNow(ActiveSimulation simulation) {
 		/**
-		 * 	EFFECTS: Return true for ability to apply events.  Returns false otherwise.
+		 * EFFECTS: Return true if the elevator is currently empty, false otherwise..
 		 */
 		Elevator elevator = simulation.getElevators()[getElevatorNumber()];
-		boolean isOpen = MotionStatus.DoorsOpen == elevator.getMotionStatus();
-		boolean isEmpty = elevator.getRequestPanel().getRequestedFloors()
-				.isEmpty();
-		return isOpen && isEmpty;
+		return elevator.getRequestPanel().getRequestedFloors().isEmpty();
 	}
 
 	public String getReason() {
 		/**
-		 * 	EFFECTS: Return the reason for the out of service event.
+		 * EFFECTS: Return the reason for the out of service event.
 		 */
 		return reason;
 	}
@@ -58,10 +58,9 @@ public class PutOutOfService extends ElevatorEvent {
 	@Override
 	public String toString() {
 		/**
-		 * 	Return a string with service event information
+		 * Return a string with service event information
 		 */
-		return String.format("%s put out of service. %s", super.toString(),
-				reason);
+		return String.format("%s put out of service. %s", super.toString(), reason);
 	}
 
 }
